c3d475762536697c58e2f8b5757b01fd0d43fe5e
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-04-27 10:07:05 -0300

    fix add room

--- src/app/pages/index/room/add-room/add-room.component.ts
+++ src/app/pages/index/room/add-room/add-room.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-add-room',
-    imports: [FormsModule, MatFormFieldModule, MatInputModule],
-    templateUrl: './add-room.component.html',
-    styleUrl: './add-room.component.scss'
+  selector: 'app-add-room',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './add-room.component.html',
+  styleUrl: './add-room.component.scss'
 })
 export class AddRoomComponent {
 
+  private router: Router = inject(Router);
+  cancelar() {
+    console.log("fdp")
+    this.router.navigate(['/rooms']);
+  }
+  addRoom() {
+    throw new Error('Method not implemented.');
+  }
 }
