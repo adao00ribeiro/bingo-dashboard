6b2cfd15ca4fa9a283785c33dc586838accc2548
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-12-08 20:55:52 -0300

    add

--- src/app/interfaces/IRoom.ts
+++ src/app/interfaces/IRoom.ts
@@ -1,7 +1,10 @@
+import { IMediaAttachment } from "./IMediaAttachment";
+
 export interface IRoom {
   id: string,
   name: string;
   ownerId: string;
 //  owner: IOwner;
  // roomSellers: any[];
+  mediaAttachment: IMediaAttachment;
 }
