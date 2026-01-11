9bc554b3a8e0746e1e5d5fc603435131124c968e
Adão Ribeiro <adao-eduardo@hotmail.com>
2026-01-10 22:02:45 -0300

    CREATE ONLINE HOUSE

---
+++ src/app/services/online-house/online-house.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IPaged } from '../../interfaces/IPaged';
+import { IOnlineHouseResponse } from '../../interfaces/response/bingo/IOnlineHouseResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnlineHouseService {
+  private url = `${environment.api}/api/v1/online-house`
+  private httpClient: HttpClient = inject(HttpClient);
+
+  GetAll(page: number, size: number): Observable<IPaged<IOnlineHouseResponse>> {
+    return this.httpClient.get<IPaged<IOnlineHouseResponse>>(this.url + `?page=${page}&size=${size}`)
+  }
+}
